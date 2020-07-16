79a0b69a8b98b6e88afda5118861e0713deb6371
suraj220487 <49473141+suraj220487@users.noreply.github.com>
2020-07-16 19:45:47 +0530

    TDD checkin

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -2,14 +2,25 @@ package org.xpdojo.bank;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.xpdojo.bank.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
-	@Disabled
+    public void newAccountShouldHaveZeroBalance()
+    {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+
+    }
+
+    @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
     }
 }
